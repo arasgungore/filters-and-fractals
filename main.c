cb900a041b20199615925c1451316398232f726c
arasgungore <sagungoren@yahoo.com.tr>
2021-03-24 13:34:53 +0300

    Updated main.c

--- main.c
+++ main.c
@@ -48,24 +48,28 @@ int main() {
 	img = readPPM("./Examples/Image Manipulation/west_1.ppm");
 	adjustBlur(img, 3);
 	writePPM("./Examples/Image Manipulation/west_1_blurred_3.ppm", img);
+	deletePPMImage(img);
 
 
 
-	PPMImage *mix = createPPMImage(750, 750, BLACK);
-	drawKochCurve(-350, -350, 350, 350, 5, mix, RED);
-	rotateImage(mix, 2);
-	drawMultipleCircles(0, 0, 300, 15, 20, mix, BLUE);
-	writePPM("./Examples/Fractals/Koch_curve_and_15_circles.ppm",mix);
+	img = createPPMImage(750, 750, BLACK);
+	drawKochCurve(-350, -350, 350, 350, 5, img, RED);
+	rotateImage(img, 2);
+	drawMultipleCircles(0, 0, 300, 15, 20, img, BLUE);
+	writePPM("./Examples/Fractals/Koch_curve_and_15_circles.ppm", img);
+	deletePPMImage(img);
 
-	mix = drawMandelbrotSet(1800, 1800);
-	drawKochCurve(-850, -850, -250, -250, 5, mix, BLUE);
-	drawCircleFractal_xyPlane(-550, 550, 300, mix, PURPLE);
-	drawSierpinskiUtil2(550, -550, 400, mix, CYAN);
-	drawSierpinski(550, -550, 200, mix, GREEN);
-	drawTree(550, 300, -90, 10, 10, mix, YELLOW);
-	writePPM("./Examples/Fractals/all_in_1.ppm",mix);
-
-	deletePPMImage(mix);
+	img = drawMandelbrotSet(1800, 1800);
+	drawKochCurve(-850, -850, -250, -250, 5, img, BLUE);
+	drawCircleFractal_xyPlane(-550, 550, 300, img, PURPLE);
+	drawSierpinskiUtil2(550, -550, 400, img, CYAN);
+	drawSierpinski(550, -550, 200, img, GREEN);
+	drawTree(550, 300, -90, 10, 10, img, YELLOW);
+	writePPM("./Examples/Fractals/all_in_1.ppm", img);
+	deletePPMImage(img);
+	
+	img = drawJulia(1920, 1080, 3);
+	writePPM("./Examples/Fractals/Julia_set_z3.ppm", img);
 
 
 	fprintf(stdout, "Image manipulation complete.\nProcess terminated successfully...");
